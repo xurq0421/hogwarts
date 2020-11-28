b2a04f88f8c372127a899f7f4658d148b42aea0e
JOJO <15919792708@163.com>
2020-11-28 13:49:34 +0800

    commit README.md,testpython.py

---
+++ pythoncode/calc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/27 13:46
+# @Author : xurq
+# @Site : 
+# @File : calc.py
+# @Software: PyCharm
+
+# 被测文件
+#　实现计算器
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    def div(self,a,b):
+        return a/b
+
+
+
---
+++ pythoncode/testpython.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/27 11:23
+# @Author : xurq
+# @Site : 
+# @File : testpython.py
+# @Software: PyCharm
+class demo:
+    def test_login(self):
+        print('test login!')
+
+
+
+
+
+
+
+
